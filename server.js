c7cc08f2e6ab0f58746eec5eced769632681a66d
Josué Makuta <makutajosue@gmail.com>
2023-02-05 20:45:48 +0100

    Feat : install dotenv

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+const { PORT } = process.env;
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server Running");
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "Hello world !" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server Running on port : ", PORT);
 });
