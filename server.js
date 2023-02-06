53ac82ef97e844e8250ab9cbcb8b6eb493fc4986
Josué Makuta <makutajosue@gmail.com>
2023-02-06 11:03:46 +0100

    Db connexion

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
 import adminRoute from "./routes/admin/admin.js";
 
 dotenv.config();
 const { PORT } = process.env;
 const app = express();
+
+//DB Connexion
+mongoose.set("strictQuery", true)
+mongoose
+  .connect("mongodb://localhost:27017/M-tech-db")
+  .then(() => {
+    console.log("DB Connected successfully");
+  })
+  .catch((error) => {
+    console.log("DB Connexion failed, Error : ", error);
+  });
+
+
 
 app.use("/admin", adminRoute);
 
