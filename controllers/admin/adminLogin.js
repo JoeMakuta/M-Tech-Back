3b923b73b30f98561813d68c1f32ee2ef752d510
Josué Makuta <makutajosue@gmail.com>
2023-02-06 15:25:28 +0100

    Feat : userName or userEmail unique to identify

--- controllers/admin/adminLogin.js
+++ controllers/admin/adminLogin.js
@@ -3,7 +3,13 @@ import adminModel from "../../models/admin/adminModel.js"
 const adminLogin = (req, res) => {
    //Validate the inputs
 
-   //
+   //check if the admin exists
+
+   //check the password
+
+   //generate token
+
+   //Send token
 }
 
 export default adminLogin
