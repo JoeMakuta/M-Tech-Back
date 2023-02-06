b6a03d099bc032d07cc1d264bd3ef8948d92c338
Josué Makuta <makutajosue@gmail.com>
2023-02-06 10:47:36 +0100

    Feat : Admin model and schema

---
+++ models/admin/adminModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const adminSchema = mongoose.Schema({
+   userName : {type : String, required : true},
+   userEmail : {type : String, required : true},
+   passWord : {type: String, required : true},
+   role : {type : Number}
+}, {collection : "admin-data"})
+
+export default adminModel = mongoose.Model("adminModel", adminSchema)
+
+
+
