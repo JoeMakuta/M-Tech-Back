1942358c208197d524fa026d7e7ce7af87eee1d6
Josué Makuta <makutajosue@gmail.com>
2023-02-07 15:36:14 +0100

    Set express headers

--- server.js
+++ server.js
@@ -23,6 +23,21 @@ await mongoose
 //The App server
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use("/admin", adminRoute);
 
 app.use(notFound);
