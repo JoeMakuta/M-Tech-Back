e76fc4c8fea50bf44a3b166d7283d47ed2b114d7
Josué Makuta <makutajosue@gmail.com>
2023-03-08 14:59:36 +0100

    Feat : product routes

---
+++ routes/product/productRoute.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+const productRoute = Router();
+
+// productRoute.get("/get", )
+// productRoute.post("/add", )
+// productRoute.put("/update", )
+// productRoute.delete("/delete", )
+
+export default productRoute
