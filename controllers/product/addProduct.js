bb0d4b70262fc4e904c294e6abbaf2056a950c01
Josué Makuta <makutajosue@gmail.com>
2023-03-08 19:41:27 +0100

    Feat : product input validation

--- controllers/product/addProduct.js
+++ controllers/product/addProduct.js
@@ -1,21 +1,28 @@
 import ProductModel from "../../models/product/productModel.js";
+import validateProduct from "../../validation/productValidation.js";
 
-const addProduct = (req, res, next) => {
+const addProduct = async (req, res, next) => {
   // Validate Inputs
+
+  const validProduct = await validateProduct(req.body);
+  if (!validProduct.error) {
+    new ProductModel({
+      ...req.body,
+    })
+      .save()
+      .then((data) => {
+        res.status(200).json({ message: "Success" });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: err.stack, err });
+      });
+  }else{
+    res.status(401).json({ err: validProduct.error?.details[0].message });
+  }
 
   // Verify if the products already exists
 
   // Save the product
-  new ProductModel({
-    ...req.body,
-  })
-    .save()
-    .then((data) => {
-      res.status(200).json({ message: "Success" });
-    })
-    .catch((err) => {
-      res.status(400).json({ message: err.stack, err });
-    });
 };
 
 export default addProduct;
--- validation/adminValidation.js
+++ validation/adminValidation.js
@@ -2,8 +2,8 @@ import Joi from "joi";
 
 const signUpValidation = (data) => {
   return Joi.object({
-    userName: Joi.string().min(3).max(255).required(),
-    userEmail: Joi.string().required().email(),
+    userName: Joi.string().trim().min(3).max(255).required(),
+    userEmail: Joi.string().trim().required().email(),
     passWord: Joi.string().min(6).max(255).required(),
     role: Joi.number().min(1).max(2).required(),
   }).validate(data);
@@ -13,8 +13,8 @@ const loginValidation = (data) => {
   return Joi.object({
     userUniqueIdentifier: Joi.alternatives()
       .try(
-        Joi.string().min(3).max(255).required(),
-        Joi.string().required().email()
+        Joi.string().trim().min(3).max(255).required(),
+        Joi.string().trim().required().email()
       )
       .required(),
     passWord: Joi.string().min(6).max(255).required(),
