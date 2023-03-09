956684d1651a5d613ffdda14cbb1748368998a94
Josué Makuta <makutajosue@gmail.com>
2023-03-09 11:05:40 +0100

    Feat : getProoducts controllers init

---
+++ controllers/product/getProduct.js
@@ -0,0 +1,18 @@
+import ProductModel from "../../models/product/productModel.js"
+
+export const getAllProducts = (req, res, next) => {
+
+}
+
+
+export const getProductById = () => {
+
+}
+
+
+export const getInLocationProducts = () => {
+
+}
+
+
+
