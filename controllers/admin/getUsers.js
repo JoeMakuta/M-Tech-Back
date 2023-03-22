1fdc0e255fd2d8206f30e010504cbf1a3f9a45bd
Josué Makuta <makutajosue@gmail.com>
2023-03-22 14:24:52 +0200

    Feat : if no user, send no user

--- controllers/admin/getUsers.js
+++ controllers/admin/getUsers.js
@@ -4,9 +4,14 @@ const GetUsers = (req, res) => {
   adminModel
     .find()
     .then((data) => {
+      if (!data[0]) {
+        res.status(404).json({ message: "No user", data });
+      }
       res.status(200).json({ message: "Success", data });
     })
-    .catch((error) => {});
+    .catch((error) => {
+      res.status(500).json({ message: "Err", error });
+    });
 };
 
 export default GetUsers;
