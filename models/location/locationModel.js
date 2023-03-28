5bc76dd58d2789c0030045d7043a521757f88079
Josué Makuta <makutajosue@gmail.com>
2023-03-28 11:29:35 +0200

    Feat : location model

---
+++ models/location/locationModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const locationSchema = mongoose.Schema(
+  {
+    productId: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      unique: true,
+      ref: "productModel",
+    },
+    locationDate: { type: Date, required: true },
+    locationDeadline: { type: Date, required: true },
+    renter: { type: String, required: true },
+    phoneRenter: { type: String, required: false },
+    locationTransport: { type: Number, required: false },
+    incomeMoney: { type: Number, required: true },
+    locationUser: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "adminModel",
+    },
+
+  },
+  { collection: "location-data", timestamps: true }
+);
+
+const LocationModel = mongoose.model("locationModel", locationSchema);
+
+export default LocationModel;
