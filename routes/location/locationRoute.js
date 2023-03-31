eebac7286304eba1ce84e590bc944a2adc9f5658
Josué Makuta <makutajosue@gmail.com>
2023-03-31 14:25:52 +0200

    Feat : delete location

--- routes/location/locationRoute.js
+++ routes/location/locationRoute.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import addLocation from "../../controllers/location/addLocation.js";
+import deleteLocation from "../../controllers/location/deleteLocation.js";
 import updateLocation from "../../controllers/location/updateLocation.js";
 
 const locationRoute = Router();
 
 locationRoute.post("/add", addLocation);
 locationRoute.post("/update/:id", updateLocation);
+locationRoute.delete("/delete/:id", deleteLocation);
 
 export default locationRoute;
